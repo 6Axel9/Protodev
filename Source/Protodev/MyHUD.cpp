dae9330ba78f6dfa56186a6d7da63fcca957a8f7
Oppilif <pippo.libardi@gmail.com>
2017-10-24 14:55:48 +0100

    Added HUD

---
+++ Source/Protodev/MyHUD.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Protodev.h"
+#include "MyHUD.h"
+
+void AMyHUD::DrawHUD()
+{
+	
+	// call superclass DrawHUD() function first
+	Super::DrawHUD();   // then proceed to draw your stuff.
+					
+						// iterate from back to front thru the list, so if we remove
+						// an item while iterating, there won't be any problems  
+	for( int c = messages.Num() - 1; c >= 0; c-- )
+	{
+		// draw the background box the right size
+		// for the message
+		float outputWidth, outputHeight, pad = 10.f;
+		GetTextSize(messages[c].message, outputWidth, outputHeight, hudFont, 1.f);
+		float messageH = outputHeight + 2.f*pad;    
+		float x = 0.f, y = c*messageH;
+		// black backing
+		DrawRect(FLinearColor::Black, x, y, Canvas->SizeX, messageH);
+		// draw our message using the hudFont
+		DrawText(messages[c].message, messages[c].color, x + pad, y + pad, hudFont);
+		// reduce lifetime by the time that passed since last 
+		// frame.     
+		messages[c].time -= GetWorld()->GetDeltaSeconds();
+		// if the message's time is up, remove it     
+		if( messages[c].time < 0 )
+		{
+			messages.RemoveAt(c);
+		}
+	}
+
+
+}
+
+void AMyHUD::DrawMessages()
+{
+	//STILL TO BE IMPLEMENTED
+}
+
+
+void AMyHUD::addMessage(Message msg)
+{
+	messages.Add(msg);
+}
---
+++ Source/Protodev/MyHUD.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "MyHUD.generated.h"
+
+struct Message
+{
+	FString message;
+	float time;
+	FColor color;
+
+	Message()
+	{
+		message = "DEFAULT MESSAGE";
+		time = 5.f;
+		color = FColor::White;
+	}
+
+	Message(FString iMessage, float iTime, FColor iColor) {
+
+		message = iMessage;     
+		time = iTime;     
+		color = iColor;
+	}
+
+
+
+};
+
+
+UCLASS()
+class PROTODEV_API AMyHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+		
+public:	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = HUDFont)
+		UFont* hudFont;
+	
+		TArray<Message> messages;
+		// Add this function to be able to draw to the HUD!   
+		virtual void DrawHUD() override;
+
+		void addMessage(Message msg);
+		void DrawMessages() ;
+	
+};

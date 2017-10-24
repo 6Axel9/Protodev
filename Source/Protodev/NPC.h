2b47938d9df34d10d74305913403f4abbdacba29
6Axel9 <oberonx10@gmail.com>
2017-10-24 14:57:07 +0100

    Added NPC

---
+++ Source/Protodev/NPC.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "NPC.generated.h"
+
+UCLASS()
+class PROTODEV_API ANPC : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ANPC();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = NPCMessage)
+
+	FString npcMessage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = NPCMessage)
+
+	UTexture2D* npcIcon;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Collision)
+
+	//USphereComponent* ProxSphere;
+
+	/*ProxSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Proximity Sphere"));*/
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};

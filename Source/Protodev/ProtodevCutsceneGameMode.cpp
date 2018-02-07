9e36f12149270d526461605672e44f9658672903
6Axel9 <oberonx10@gmail.com>
2018-02-07 22:12:17 +0000

    Cutscene and new Cutscene Widget added and working when starting a new game

---
+++ Source/Protodev/ProtodevCutsceneGameMode.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Protodev.h"
+#include "ProtodevCutsceneGameMode.h"
+
+
+void AProtodevCutsceneGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	ChangeCutsceneWidget(StartingWidgetClass);
+}
+
+void AProtodevCutsceneGameMode::ChangeCutsceneWidget(TSubclassOf<UUserWidget> NewWidgetClass)
+{
+	if (CurrentWidget != nullptr)
+	{
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
